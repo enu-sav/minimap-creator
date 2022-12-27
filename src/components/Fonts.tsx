b896282321810a1ee5fcab9e95b81f5b29796c7f
Martin Ždila <m.zdila@gmail.com>
2022-12-27 16:35:22 +0100

    fonts cleanup

--- src/components/Fonts.tsx
+++ src/components/Fonts.tsx
@@ -3,9 +3,9 @@ import { Font, FontSet } from "jsxnik/mapnikConfig";
 export function Fonts() {
   return (
     <FontSet name="regular">
-      <Font faceName="PT Sans Regular" />
-      <Font faceName="Fira Sans Condensed Regular" />
       <Font faceName="Noto Sans Regular" />
+      <Font faceName="Noto Sans SC Regular" />
+      <Font faceName="Noto Sans JP Regular" />
     </FontSet>
   );
 }
